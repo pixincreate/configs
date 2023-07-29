73ce90d043141387801656cf1ed940c0a6ebe249
PiX <69745008+pixincreate@users.noreply.github.com>
2023-07-29 20:30:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Configs
+
+This repo contains all of my development configs. I never had a backup of these before and hence this repo.
+
+## Contents
+
+- starship.toml: Tokyo-Night theme
+- VSCode profile: Tokyo-Night theme with the extensions that I use
+- Unixified powershell profile
+
+## Powershell for better life (Admin / sudo recommended)
+
+### One line powershell profile installer
+
+```pwsh
+irm "https://github.com/pixincreate/configs/raw/main/pwsh_profile.ps1" | iex
+```
+
+### Winutil
+
+```pwsh
+irm https://christitus.com/win | iex
+```
+
+#### Installation
+
+In Powershell, execute:
+
+```pwsh
+echo "irm https://christitus.com/win | iex" > source.ps1
+Install-Module -Name ps2exe -RequiredVersion 1.0.4 
+2exe .\source.ps1 .\winutil.exe
+```
+
+Put the `winutil.exe` in a place where you wish the file to be and copy-paste its path in environment variables.
+
+#### Usage
+
+Open `terminal / cmd / powershell`, execute `winutil` to run the application
---
+++ pwsh_profile.ps1
@@ -0,0 +1,223 @@
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# This file was created by PiX.
+# Date: 29/07/2023
+# Description: This file contains the profile for PowerShell Core along with some useful functions and aliases to make working with powershell easier and
+# intutive with that of Unis based OSes.
+# Since working on Mac for office work has greatly increased my productivity, I wanted to have a similar experience on Windows as well and hence 
+# I wanted to have a similar unix based experience on Windows as well. Started with re-writing small commands such as `touch` and `ls` and then
+# found ChrisTitus Tech's powershell profile and decided to add his work on top of my customization.
+# This file also includes starship profile as well.
+
+# Credit:
+#   - ChrisTitus
+# --------------------------------------------------------------------------------------------------------------------------------------------
+
+# Find out if the current user identity is elevated (has admin rights)
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object Security.Principal.WindowsPrincipal $identity
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# If so and the current host is a command line, then change to red color 
+# as warning to user that they are operating in an elevated context
+# Useful shortcuts for traversing directories
+function cd... { Set-Location ..\.. }
+function cd.... { Set-Location ..\..\.. }
+
+# Compute file hashes - useful for checking successful downloads 
+function md5 { Get-FileHash -Algorithm MD5 $args }
+function sha1 { Get-FileHash -Algorithm SHA1 $args }
+function sha256 { Get-FileHash -Algorithm SHA256 $args }
+
+# Quick shortcut to start notepad
+function n { notepad $args }
+
+# Drive shortcuts
+function HKLM: { Set-Location HKLM: }
+function HKCU: { Set-Location HKCU: }
+function Env: { Set-Location Env: }
+
+# Creates drive shortcut for Work Folders, if current user account is using it
+if (Test-Path "$env:USERPROFILE\Work Folders") {
+    New-PSDrive -Name Work -PSProvider FileSystem -Root "$env:USERPROFILE\Work Folders" -Description "Work Folders"
+    function Work: { Set-Location Work: }
+}
+
+# Set up command prompt and window title. Use UNIX-style convention for identifying 
+# whether user is elevated (root) or not. Window title shows current version of PowerShell
+# and appends [ADMIN] if appropriate for easy taskbar identification
+function prompt { 
+    if ($isAdmin) {
+        "[" + (Get-Location) + "] # " 
+    } else {
+        "[" + (Get-Location) + "] $ "
+    }
+}
+
+$Host.UI.RawUI.WindowTitle = "PowerShell {0}" -f $PSVersionTable.PSVersion.ToString()
+if ($isAdmin) {
+    $Host.UI.RawUI.WindowTitle += " [ADMIN]"
+}
+
+# Does the the rough equivalent of dir /s /b. For example, dirs *.png is dir /s /b *.png
+function dirs {
+    if ($args.Count -gt 0) {
+        Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
+    } else {
+        Get-ChildItem -Recurse | Foreach-Object FullName
+    }
+}
+
+# Simple function to start a new elevated process. If arguments are supplied then 
+# a single command is started with admin rights; if not then a new admin instance
+# of PowerShell is started.
+function admin {
+    if ($args.Count -gt 0) {   
+        $argList = "& '" + $args + "'"
+        Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
+    } else {
+        Start-Process "$psHome\powershell.exe" -Verb runAs
+    }
+}
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
+# with elevated rights. 
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
+
+
+# Make it easy to edit this profile once it's installed
+function Edit-Profile {
+    if ($host.Name -match "ise") {
+        $psISE.CurrentPowerShellTab.Files.Add($profile.CurrentUserAllHosts)
+    } else {
+        notepad $profile.CurrentUserAllHosts
+    }
+}
+
+# We don't need these any more; they were just temporary variables to get to $isAdmin. 
+# Delete them to prevent cluttering up the user profile. 
+Remove-Variable identity
+Remove-Variable principal
+
+Function Test-CommandExists {
+    Param ($command)
+    $oldPreference = $ErrorActionPreference
+    $ErrorActionPreference = 'SilentlyContinue'
+    try { if (Get-Command $command) { RETURN $true } }
+    Catch { Write-Host "$command does not exist"; RETURN $false }
+    Finally { $ErrorActionPreference = $oldPreference }
+} 
+#
+# Aliases
+#
+# If your favorite editor is not here, add an elseif and ensure that the directory it is installed in exists in your $env:Path
+#
+if (Test-CommandExists nvim) {
+    $EDITOR='nvim'
+} elseif (Test-CommandExists pvim) {
+    $EDITOR='pvim'
+} elseif (Test-CommandExists vim) {
+    $EDITOR='vim'
+} elseif (Test-CommandExists vi) {
+    $EDITOR='vi'
+} elseif (Test-CommandExists code) {
+    $EDITOR='code'
+} elseif (Test-CommandExists notepad) {
+    $EDITOR='notepad'
+} elseif (Test-CommandExists notepad++) {
+    $EDITOR='notepad++'
+} elseif (Test-CommandExists sublime_text) {
+    $EDITOR='sublime_text'
+}
+Set-Alias -Name vim -Value $EDITOR
+
+
+function ll { Get-ChildItem -Path $pwd -File }
+function g { Set-Location $HOME\Documents\Github }
+function gcom {
+    git add .
+    git commit -m "$args"
+}
+function lazyg {
+    git add .
+    git commit -m "$args"
+    git push
+}
+function Get-PubIP {
+    (Invoke-WebRequest http://ifconfig.me/ip ).Content
+}
+function uptime {
+    #Windows Powershell    
+    Get-WmiObject win32_operatingsystem | Select-Object csname, @{
+        LABEL      = 'LastBootUpTime';
+        EXPRESSION = { $_.ConverttoDateTime($_.lastbootuptime) }
+    }
+}
+function reload-profile {
+    & $profile
+}
+function find-file($name) {
+    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+        $place_path = $_.directory
+        Write-Output "${place_path}\${_}"
+    }
+}
+function unzip ($file) {
+    Write-Output("Extracting", $file, "to", $pwd)
+    $fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object { $_.FullName }
+    Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+function ix ($file) {
+    curl.exe -F "f:1=@$file" ix.io
+}
+function grep($regex, $dir) {
+    if ( $dir ) {
+        Get-ChildItem $dir | select-string $regex
+        return
+    }
+    $input | select-string $regex
+}
+function df {
+    get-volume
+}
+function sed($file, $find, $replace) {
+    (Get-Content $file).replace("$find", $replace) | Set-Content $file
+}
+function which($name) {
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
+function export($name, $value) {
+    set-item -force -path "env:$name" -value $value;
+}
+function pkill($name) {
+    Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
+}
+function pgrep($name) {
+    Get-Process $name
+}
+# touch command in powershell
+function touch {
+    if((Test-Path -Path ($args[0])) -eq $false) {
+        Set-Content -Path ($args[0]) -Value ($null)
+    } else {
+        (Get-Item ($args[0])).LastWriteTime = Get-Date 
+    }
+}
+# mkdir command in powershell
+function mkdir {
+    New-Item "$args" -ItemType Directory
+}
+
+
+## Final Line to set prompt AKA invoke starship
+Invoke-Expression (&starship init powershell)
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
