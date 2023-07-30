3a8a0bb8301db12725ea013f7e1de3ae4ce02a04
PiX <69745008+pixincreate@users.noreply.github.com>
2023-07-30 12:17:25 +0530

    Remove sudo for gsudo

--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -67,24 +67,6 @@ function dirs {
     }
 }
 
-# Simple function to start a new elevated process. If arguments are supplied then 
-# a single command is started with admin rights; if not then a new admin instance
-# of PowerShell is started.
-function admin {
-    if ($args.Count -gt 0) {   
-        $argList = "& '" + $args + "'"
-        Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
-    } else {
-        Start-Process "$psHome\powershell.exe" -Verb runAs
-    }
-}
-
-# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
-# with elevated rights. 
-Set-Alias -Name su -Value admin
-Set-Alias -Name sudo -Value admin
-
-
 # Make it easy to edit this profile once it's installed
 function Edit-Profile {
     if ($host.Name -match "ise") {
