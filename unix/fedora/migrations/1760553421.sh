f138e24e3ec5bb3a4daea1ea9aa005e470f11de3
PiX <69745008+pixincreate@users.noreply.github.com>
2025-10-16 00:50:50 +0530

    fix: add missing package `trash-cli`

---
+++ unix/fedora/migrations/1760553421.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Install trash-cli
+echo "Running migration: Install trash-cli"
+
+omaforge-pkg-add trash-cli
+
+echo "Migration completed: trash-cli has been installed successfully"
+
